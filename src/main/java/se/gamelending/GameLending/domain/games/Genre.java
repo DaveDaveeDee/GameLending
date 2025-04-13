563442854c02d4540e0a81f78434586b39dfc2f5
david rydman <david.rydman@iths.se>
2025-04-13 18:57:01 +0200

    Created Genre entity

---
+++ src/main/java/se/gamelending/GameLending/domain/games/Genre.java
@@ -0,0 +1,28 @@
+package se.gamelending.GameLending.domain.games;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "genres")
+public class Genre {
+    @Id
+    @Column(name = "genres")
+    private String genre;
+
+
+    public Genre() {
+    }
+
+    public Genre(String genre) {
+        this.genre = genre;
+    }
+
+    // getters and setters
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
