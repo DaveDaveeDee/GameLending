374d0a9206b51a78d69a8ce0922f9b5f85cf29c5
david rydman <david.rydman@iths.se>
2025-04-13 15:30:41 +0200

    Initial commit

---
+++ src/main/java/se/gamelending/GameLending/GameLendingApplication.java
@@ -0,0 +1,13 @@
+package se.gamelending.GameLending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameLendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLendingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/gamelending/GameLending/GameLendingApplicationTests.java
@@ -0,0 +1,13 @@
+package se.gamelending.GameLending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameLendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
