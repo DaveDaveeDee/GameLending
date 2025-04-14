69f082978b379df47ad08fc848a4408aacf9abe8
david rydman <david.rydman@iths.se>
2025-04-14 15:58:42 +0200

    Created Game entity class

---
+++ src/main/java/se/gamelending/GameLending/domain/games/Game.java
@@ -0,0 +1,148 @@
+package se.gamelending.GameLending.domain.games;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import se.gamelending.GameLending.domain.users.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Entity
+@Table(name = "games")
+public class Game {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
+
+    @Column(name = "name")
+    @NotEmpty
+    private String name;
+
+    @Column(name = "console")
+    @NotEmpty
+    private String console;
+
+    @Column(name = "genres")
+    @OneToMany(
+            fetch = FetchType.EAGER,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @NotNull
+    private List<Genre> genres;
+
+    @Column(name = "publishing year")
+    @NotNull
+    private int publishingYear;
+
+    @Column(name = "publisher")
+    @NotEmpty
+    private String publisher;
+
+    @JoinColumn(name = "owner")
+    @ManyToOne(
+            fetch = FetchType.EAGER,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @NotNull
+    private User owner;
+
+    @JoinColumn(name = "holder")
+    @ManyToOne(
+            fetch = FetchType.EAGER,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @NotNull
+    private User holder;
+
+    @Column(name = "lendingdate", columnDefinition = "DATETIME")
+    private LocalDateTime lendingDate;
+
+    public Game(String name, String console, List<Genre> genres, int publishingYear,
+                String publisher, User owner, User holder, LocalDateTime lendingDate) {
+        this.name = name;
+        this.console = console;
+        this.genres = genres;
+        this.publishingYear = publishingYear;
+        this.publisher = publisher;
+        this.owner = owner;
+        this.holder = holder;
+        this.lendingDate = lendingDate;
+    }
+
+    public Game() {
+    }
+
+    // Getters and Setters
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getConsole() {
+        return console;
+    }
+
+    public void setConsole(String console) {
+        this.console = console;
+    }
+
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        this.genres = genres;
+    }
+
+    public int getPublishingYear() {
+        return publishingYear;
+    }
+
+    public void setPublishingYear(int publishingYear) {
+        this.publishingYear = publishingYear;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public User getHolder() {
+        return holder;
+    }
+
+    public void setHolder(User holder) {
+        this.holder = holder;
+    }
+
+    public LocalDateTime getLendingDate() {
+        return lendingDate;
+    }
+
+    public void setLendingDate(LocalDateTime lendingDate) {
+        this.lendingDate = lendingDate;
+    }
+}
